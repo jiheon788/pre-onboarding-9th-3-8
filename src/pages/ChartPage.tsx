e8134f58540685134492ab4eedbbbec1f85835b0
jiheon788 <jiheon788@ajou.ac.kr>
2023-03-15 02:33:22 +0900

    feat: 시계열 차트 만들기

--- src/pages/ChartPage.tsx
+++ src/pages/ChartPage.tsx
@@ -1,12 +1,115 @@
 import { getTimeSeriesData } from '@/api/mock';
-import { useEffect } from 'react';
-const ChartPage = () => {
+import { useEffect, useState } from 'react';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  Filler,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+  Filler,
+);
+
+export const ChartPage = () => {
+  const [areaData, setAreaData] = useState<number[]>([]);
+  const [barData, setBarData] = useState<number[]>([]);
+  const [labels, setLabels] = useState<string[]>([]);
+
   useEffect(() => {
     getTimeSeriesData().then((res) => {
-      console.log(res.data.response);
+      setLabels(Object.keys(res.data.response));
+
+      setAreaData(
+        Object.keys(res.data.response).map(
+          (key) => res.data.response[key].value_area,
+        ),
+      );
+
+      setBarData(
+        Object.keys(res.data.response).map(
+          (key) => res.data.response[key].value_bar,
+        ),
+      );
     });
   }, []);
-  return <>ChartPage</>;
+
+  useEffect(() => {
+    console.log(areaData);
+  }, [areaData]);
+
+  const options = {
+    responsive: true,
+    interaction: {
+      mode: 'index' as const,
+      intersect: false,
+    },
+    stacked: false,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart - Multi Axis',
+      },
+    },
+    scales: {
+      y: {
+        type: 'linear' as const,
+        display: true,
+        position: 'left' as const,
+      },
+      y1: {
+        type: 'linear' as const,
+        display: true,
+        position: 'right' as const,
+        grid: {
+          drawOnChartArea: false,
+        },
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        type: 'line' as const,
+        fill: true,
+        label: 'value_area',
+        data: areaData,
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        yAxisID: 'y',
+      },
+      {
+        type: 'bar' as const,
+        label: 'value_bar',
+        backgroundColor: 'rgb(75, 192, 192)',
+        data: barData,
+        borderColor: 'white',
+        borderWidth: 2,
+        yAxisID: 'y1',
+      },
+    ],
+  };
+
+  return <Chart type="line" data={data} options={options} />;
 };
 
 export default ChartPage;
